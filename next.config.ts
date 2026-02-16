ec566f7b12e567bbdd5eee1ecadc2e41abdc9944
Gargamel988 <omeraydin1.web@gmail.com>
2026-02-16 22:01:49 +0300

    migrate to opennext cloudflare

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Görsel optimizasyonu
-  output: "export",
+  output: "standalone",
 
   images: {
     remotePatterns: [
